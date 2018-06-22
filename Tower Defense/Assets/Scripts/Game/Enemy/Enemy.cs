7a89f26adc7407d55eea83bf6880ea9c46edd437
tribustix <tribustix@gmail.com>
2018-06-22 17:08:22 +0200

    Added Enemies
    Added Enemies that wifollow the route that we set before

---
+++ Tower Defense/Assets/Scripts/Game/Enemy/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float maxHealth = 100f;
+	public float health = 100f;
+	public float moveSpeed = 3f;
+
+	public int goldDrop = 10;
+	public int pathIndex = 0;
+	public int wayPointIndex = 0;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+		if(wayPointIndex < WayPointManager.Instance.Paths[pathIndex].WayPoints.Count){
+			UpdateMovement();
+		}else{
+			OnGoToLastWayPoint();
+		}
+
+	}
+
+	void OnGoToLastWayPoint () {
+		Die();
+	}
+
+	void Die () {
+		if(gameObject != null){
+			Destroy(gameObject);
+		}
+	}
+
+	public void TakeDamage (float amountOfDamage) {
+		health -= amountOfDamage;
+
+		if(health <= 0){
+			Die();
+		}
+	}
+
+	public void UpdateMovement () {
+		Vector3 targetPosition = WayPointManager.Instance.Paths[pathIndex].WayPoints[wayPointIndex].position;
+
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+		transform.LookAt(targetPosition);
+		
+		// If the enemy is very close to the targetWaypoint, set the next waypoint as target
+		if(Vector3.Distance(transform.position, targetPosition) < .1f){
+			wayPointIndex++;
+		}
+	}
+
+	
+}
