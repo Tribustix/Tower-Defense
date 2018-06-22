35e7e54837448a4b18a3bb5a7cc81aef631ab684
tribustix <tribustix@gmail.com>
2018-06-22 19:01:45 +0200

    Added functionality to move enemies' wheels

---
+++ Tower Defense/Assets/Scripts/Utility/LoopRate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopRate : MonoBehaviour {
+
+	public Vector3 roation;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		transform.Rotate(roation * Time.deltaTime);
+	}
+}
