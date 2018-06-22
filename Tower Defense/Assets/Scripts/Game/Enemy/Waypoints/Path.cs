d11303f8815b7b4cb5025281d17a0362249a0e09
tribustix <tribustix@gmail.com>
2018-06-22 15:56:07 +0200

    Added a Functionality to make paths
    Added a functionality to make paths so the enemies can follow a rute across the map.

---
+++ Tower Defense/Assets/Scripts/Game/Enemy/Waypoints/Path.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] public class Path {
+		public List<Transform> WayPoints = new List<Transform>();
+	}
