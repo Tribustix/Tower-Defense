226c403148d8925120fab4c116a425a22d07cff5
tribustix <tribustix@gmail.com>
2018-06-23 17:26:20 +0200

    Added a Tower Manager
    Added a Tower Manager to buy and build Towers from the UI

---
+++ Tower Defense/Assets/Scripts/Game/Tower/TowerManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+
+[System.Serializable] public struct TowerCost {
+	public TowerType TowerType;
+	public int cost;
+}
+
+
+public class TowerManager : MonoBehaviour {
+
+	public static TowerManager Instance;
+
+	public GameObject stoneTowerPrefab;
+	public GameObject fireTowerPrefab;
+	public GameObject iceTowerPrefab;
+
+	public List<TowerCost> TowerCosts = new List <TowerCost>();
+
+	void Awake () {
+		Instance = this;	
+	}	
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	public void CreateNewTower (GameObject slotToFill, TowerType towerType) {
+		
+		switch(towerType){
+
+			case TowerType.Stone:
+			Instantiate(stoneTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+			slotToFill.gameObject.SetActive(true);
+			break;
+
+			case TowerType.Fire:
+			Instantiate(fireTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+			slotToFill.gameObject.SetActive(true);
+			break;
+
+			case TowerType.Ice:
+			Instantiate(iceTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+			slotToFill.gameObject.SetActive(true);
+			break;
+		}
+	}
+
+
+	//LINQ utility method to get the price of a tower.
+	public int GetTowerPrice(TowerType towerType){
+		return (from TowerCost in TowerCosts where TowerCost.TowerType == towerType select TowerCost.cost).FirstOrDefault();
+	}
+}
